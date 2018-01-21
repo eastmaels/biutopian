c80f67ba8ef8e85a3e1c10e751304ba1aff20c28
East <eastmaels@gmail.com>
2018-01-21 17:52:47 +0800

    changed delimeter from comma to double pipes (||)

--- delegation_selfcheck.js
+++ delegation_selfcheck.js
@@ -77,7 +77,7 @@ sql.connect(config, function (err) {
 
     const filetstamp = dateFormat(now, "UTC:yyyymmdd_HHMMss");
     const outputCsv = `analysis/delegation_self-check_${delegatee}_${filetstamp}.csv`;
-    fs.writeFileSync(outputCsv, 'Voter,Weight,% Weight,Author,Rep,Permlink,vote_tstamp,Total Payout,Pending Payout,Curator Payout,Total Payout Value,Total Reward Shares,Ratio,Delegatee Vote Value,Vote Count,Created,Vote Date,Comments,json_metadata\r\n');
+    fs.writeFileSync(outputCsv, 'Voter||Weight||% Weight||Author||Rep,Permlink||vote_tstamp||Total Payout||Pending Payout||Curator Payout||Total Payout Value||Total Reward Shares||Ratio||Delegatee Vote Value||Vote Count||Created||Vote Date||Comments||json_metadata\r\n');
     result.recordset.forEach(function(item) {
 
       var active_votes = JSON.parse(item.active_votes);
@@ -99,7 +99,7 @@ sql.connect(config, function (err) {
       const vote_date = dateFormat(item.votes_tstamp, "UTC:yyyy-mm-dd");
       const created = dateFormat(item.created, "UTC:yyyy-mm-dd HH:MM:ss");
 
-      var dataToWrite = `${item.votes_voter},${item.votes_weight},${weight_in_percent},${item.votes_author},${item.rep},${item.votes_permlink},${vote_tstamp},${item.total_payout},${item.pending_payout_value},${item.curator_payout_value},${item.total_payout_value},${total_rshares},${ratio},${delegateeVoteValue},${active_votes.length},${created},${vote_date},${item.children},${item.json_metadata}\r\n`
+      var dataToWrite = `${item.votes_voter}||${item.votes_weight}||${weight_in_percent}||${item.votes_author}||${item.rep}||${item.votes_permlink}||${vote_tstamp}||${item.total_payout}||${item.pending_payout_value}||${item.curator_payout_value}||${item.total_payout_value}||${total_rshares}||${ratio}||${delegateeVoteValue}||${active_votes.length}||${created}||${vote_date}||${item.children}||${item.json_metadata}\r\n`
       fs.appendFileSync(outputCsv, dataToWrite);
     });
 
