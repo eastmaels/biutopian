987af2c110be10010a981c2dd16e03de3c6f0570
East <eastmaels@gmail.com>
2018-01-21 19:33:20 +0800

    updated table header to match column of json_metadata

--- delegation_selfcheck.js
+++ delegation_selfcheck.js
@@ -102,7 +102,7 @@ sql.connect(config, function (err) {
 	  const json_metadata = JSON.parse(item.record_json_metadata);
 	  const tags = '"{""tags"":[""' + json_metadata.tags.join('"",""') + '""]}"';
 
-      var dataToWrite = `${item.votes_voter}||${item.votes_weight}||${weight_in_percent}||${item.votes_author}||${item.rep}||${item.votes_permlink}||${vote_tstamp}||${item.total_payout}||${item.pending_payout_value}||${item.curator_payout_value}||${item.total_payout_value}||${total_rshares}||${ratio}||${delegateeVoteValue}||${active_votes.length}||${created}||${vote_date}||${tags}||${item.children}\r\n`
+      var dataToWrite = `${item.votes_voter}||${item.votes_weight}||${weight_in_percent}||${item.votes_author}||${item.rep}||${item.votes_permlink}||${vote_tstamp}||${item.total_payout}||${item.pending_payout_value}||${item.curator_payout_value}||${item.total_payout_value}||${total_rshares}||${ratio}||${delegateeVoteValue}||${active_votes.length}||${created}||${vote_date}||${item.children}||${tags}\r\n`
       fs.appendFileSync(outputCsv, dataToWrite);
     });
 
